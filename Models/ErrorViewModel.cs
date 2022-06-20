0e420f5bab9026dfb62cd55dd993d2a6a8a2b01c
Taha Toy <mttoy45@gmail.com>
2022-06-20 13:06:10 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TualWear.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
